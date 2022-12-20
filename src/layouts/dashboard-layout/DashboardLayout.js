5418481281036d16720e5b6bfdc8a764ba604eff
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2022-12-21 02:02:23 +0300

    create dashboard layout page

---
+++ src/layouts/dashboard-layout/DashboardLayout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Header from "../../companents/shared/header/Header";
+
+function DashboardLayout() {
+  return (
+    <div className="App">
+      <Header title={"E-Commerce"} />
+    </div>
+  );
+}
+
+export default DashboardLayout;
