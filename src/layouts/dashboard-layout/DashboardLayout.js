acb9d911c8aa005c31ffca42e7a43b819fa6cfeb
Şenay Şentürk <senaysenturk@outlook.com>
2022-12-21 23:00:33 +0300

    #12 | add product list and designed card
    #12 | add product list and designed card

--- src/layouts/dashboard-layout/DashboardLayout.js
+++ src/layouts/dashboard-layout/DashboardLayout.js
@@ -1,8 +1,8 @@
 import React from "react";
 import List from "../../components/product/list/List";
-
 import Header from "../../components/shared/header/Header";
 import Slider from "../../components/shared/slider/Slider";
+
 function DashboardLayout() {
   return (
     <div className="App">
