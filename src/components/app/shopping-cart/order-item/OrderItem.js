5da8e39981eca40531278a9737ed62bf3f50a727
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2022-12-26 22:01:36 +0300

    Orders page design #11 add order item code

---
+++ src/components/app/shopping-cart/order-item/OrderItem.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { MdOutlineLocalShipping } from "react-icons/md";
+import { RiDeleteBinLine } from "react-icons/ri";
+
+import "./style.scss";
+
+export const OrderDetail = () => {
+  let counter = 1;
+
+  const increaseValue = () => {
+    counter++;
+    console.log(counter);
+    document.getElementById("number").innerText = counter;
+  };
+
+  const decreaseValue = () => {
+    if (counter > 1) {
+      counter--;
+      document.getElementById("number").innerText = counter;
+    }
+  };
+  return (
+    <>
+      <div className="order-card">
+        <div className="order-header">
+          <header>Sepet (1)</header>
+        </div>
+        <div className="order-item-card">
+          <div className="item-image">
+            <img src="https://02b3ab.cdn.akinoncloud.com/products/2022/11/02/42945/f713dc12-8868-47b0-b64a-aa081b93ce92_size220x220_cropCenter.jpg" />
+          </div>
+          <div className="item-content-main">
+            <div className="item-main">
+              <div className="item-product">
+                <a>Kael Kadın Bornoz Ekru</a>
+                <span className="delete-icon">
+                  <RiDeleteBinLine />
+                </span>
+              </div>
+              <div className="item-attributes-holder">
+                <span className="item-attribute-value">Ekru</span>
+                <p className="item-attribute"></p>
+                <span className="item-attribute-value">S</span>
+              </div>
+              <div className="item-middle">
+                <span className="shipping-icon">
+                  <MdOutlineLocalShipping />
+                </span>
+                <p>
+                  <span>29 Aralık Perşembe</span>Tarihinde Kargoda
+                </p>
+              </div>
+              <div className="item-bottom">
+                <div className="item-quantity">
+                  <div className="item-quantity-wrapper">
+                    <a
+                      className="item-quantity-button item-decrease-button"
+                      onclick="decreaseValue()"
+                    >
+                      <span>-</span>
+                    </a>
+                    <span className="item-quantity-input" id="number">
+                      1
+                    </span>
+                    <a
+                      className="item-quantity-button item-increase-button"
+                      onclick="increaseValue()"
+                    >
+                      <span>+</span>
+                    </a>
+                  </div>
+                </div>
+                <div className="item-price">
+                  <span className="actual-price">1.699,99 TL</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OrderDetail;
