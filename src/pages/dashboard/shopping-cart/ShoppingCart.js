8d3072918bcfdb0d6c841876770eda1560c09ff1
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2022-12-26 12:15:21 +0300

    Orders page design #11 created shopping cart page

--- src/pages/dashboard/shopping-cart/ShoppingCart.js
+++ src/pages/dashboard/shopping-cart/ShoppingCart.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
+import "./style.scss";
+import "../../../utilities.scss";
+
+import OrderSummary from "../../../components/app/shopping-cart/order-summary/OrderSummary";
+import CardDetails from "../../../components/shared/card/card-details/CardDetails";
 export const ShoppingCart = () => {
   return (
-    <div>ShoppingCart</div>
+    <>
+    <div>
+      <OrderSummary />
+      <CardDetails />
+    </div>
+    </>
   )
 }
 
