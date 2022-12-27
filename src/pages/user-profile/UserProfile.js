167a10df46f225d8b230028c0f222964c86f90bd
hicranertugral <hicranertugral@gmail.com>
2022-12-28 00:00:48 +0300

    #29 Profile detail page create

--- src/pages/user-profile/UserProfile.js
+++ src/pages/user-profile/UserProfile.js
@@ -4,65 +4,65 @@ import { Link, useNavigate } from "react-router-dom";
 const UserProfile = () => {
   return (
     <div className="user-profile">
-      <div className="user-info"></div>
-      <div className="user-summary">
-        <h2 id="username">senaysenturk</h2>
+
+      <div className="user-info">
+        <label  id="username">senaysenturk</label>
       </div>
 
-      <div className="full-name" />
-      <label for="full-name">Firts Name</label>
-      <input type="text" className="full-name" name="full-name" placeholder="Full Name" />
-      <div className="foot-notes">
-        <p>
-          Help people discover your account using a name you're familiar with,
-          such as your first and last name, nickname, or business name.
-          <br />
-          You can only change your name twice in 14 days.
-        </p>
+      <div className="first-name">
+        <label for="first-name">First Name</label>
+        <input
+          type="text"
+          className="first-name"
+          name="first-name"
+          placeholder="Full Name"
+        />
       </div>
 
-      <div className="last-name" />
-      <label for="last-name">Last Name</label>
-      <input type="text" className="last-name" name="last-name" placeholder="Last Name" />
-      <div className="foot-notes">
-        <p>
-          Help people discover your account using a name you're familiar with,
-          such as your first and last name, nickname, or business name.
-          <br />
-          You can only change your name twice in 14 days.
-        </p>
+      <div className="last-name">
+        <label for="last-name">Last Name</label>
+        <input
+          type="text"
+          className="last-name"
+          name="last-name"
+          placeholder="Last Name"
+        />
       </div>
 
-      <div className="user-name" />
-      <label for="user-name">UserName</label>
-      <input type="text" id="username" name="user-name" placeholder="User Name"/>
-      <div className="foot-notes">
-        <p>
-          In most cases, you will be able to change your username back to{" "}
-          <span id="username">senaysenturk</span> <Link>learn more</Link>
-        </p>
+      <div className="user-name">
+        <label for="user-name">UserName</label>
+        <input
+          type="text"
+          id="username"
+          name="user-name"
+          placeholder="User Name"
+        />
       </div>
 
-      <div className="website" />
-      <label for="website">Name</label>
-      <input type="url" className="website" name="website" placeholder="Website" />
-      <div className="foot-notes">
-        <p>
-        You can only edit your links on mobile devices. To change websites in your bio, visit the Instagram app and edit your profile.
-        </p>
+      <div id="email">
+        <label for="email">E-Mail</label>
+        <input type="user-email" id="email" name="email" placeholder="E-mail" />
       </div>
 
-      <div className="bio" />
-      <label for="bio">Name</label>
-      <input type="url" className="website" name="website" placeholder="Website" />
-      <div className="foot-notes">
-        <p>
-        You can only edit your links on mobile devices. To change websites in your bio, visit the Instagram app and edit your profile.
-        </p>
+      <div className="phone">
+        <label for="phone">Phone Number</label>
+        <input type="url" id="phone" name="phone" placeholder="Phone Number" />
       </div>
 
+      <div className="gender">
+      <label for="gender">Gender</label>
+      <select id="gender" name="gender" >
+        <option>Man</option>
+        <option>Woman</option>
+        <option>Other</option>
+        </select>
+
+      </div>
 
     </div>
+
+    
+
   );
 };
 
