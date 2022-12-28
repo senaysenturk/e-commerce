cf946bec1ca97f7561c842993c6ed77dc3562f09
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2022-12-29 00:07:43 +0300

    started address form

--- src/components/app/shopping-cart/address-form/AddressForm.js
+++ src/components/app/shopping-cart/address-form/AddressForm.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+
+import "./style.scss";
 
 export const AddressForm = () => {
   return (
-    <div>AddressForm</div>
-  )
-}
+    <>
+      <div className="checkout-addresses">
+        <div className="checkout-addresses-head">
+          <header>Teslimat Adresi</header>
+        </div>
+        <div className="address-form">
+          <input
+            type="text"
+            id="fname"
+            name="firstname"
+            placeholder="Your name.."
+          />
 
-export default AddressForm
+          <input
+            type="text"
+            id="lname"
+            name="lastname"
+            placeholder="Your last name.."
+          />
+
+          <p>Address Informations</p>
+
+          <select id="country" name="country">
+            <option value="australia">Australia</option>
+            <option value="canada">Canada</option>
+            <option value="usa">USA</option>
+          </select>
+
+          <textarea
+            id="subject"
+            name="subject"
+            placeholder="Write something.."
+            //style="height:200px"
+          ></textarea>
+
+          <input
+            type="text"
+            id="lname"
+            name="lastname"
+            placeholder="Your last name.."
+          />
+
+          <input
+            type="text"
+            id="lname"
+            name="lastname"
+            placeholder="Your address name.."
+          />
+
+          <div className="add-button">
+            <button className="btn btn-primary">Save</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddressForm;
