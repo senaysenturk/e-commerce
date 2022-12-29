7ab7857d173797a11be5abce669dbf2e420bb21e
ipekgulsencelik <64319688+ipekgulsencelik@users.noreply.github.com>
2022-12-29 23:21:30 +0300

    Update AuthContext

--- src/contexts/auth/AuthContext.js
+++ src/contexts/auth/AuthContext.js
@@ -1,23 +1,19 @@
 import { createContext, useContext, useState } from "react";
+import { postUser } from "../../network/requests/auth/register";
 
-const AuthContext = createContext({});
+const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState({});
+  const [user, setUser] = useState({});
+  console.log("User", user);
+  const addUser = async () => {
+    const response = await postUser(user);
+    console.log("Reponse: " + response);
+  };
 
-    return (
-        <AuthContext.Provider value={{ auth, setAuth }}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  const values = { user, setUser, addUser };
 
-export default AuthContext;
-
-/* 
-export const useAuth = () => {
-    const { auth } = useContext(AuthContext);
-    useDebugValue(auth, auth => auth?.user ? "Logged In" : "Logged Out")
-
-    return useContext(AuthContext);
-} */
+  return (
+    <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
+  );
+};
