7213d8eeae9161e6ae98f0071ef6e7990b8b782f
Şenay ŞENTÜRK <senaysenturk@outlook.com>
2022-12-31 14:35:21 +0300

    #38 | change color's radio button css

--- src/components/shared/card/CardDetail.js
+++ src/components/shared/card/CardDetail.js
@@ -61,45 +61,12 @@ const CardDetail = ({ path }) => {
           </p>
           <div className="colors">
             Colors:
-            <input type="radio" name="color" id="red" value="red" />
             <label for="red">
               <span class="red"></span>
             </label>
-            <input type="radio" name="color" id="green" />
-            <label for="green">
-              <span class="green"></span>
-            </label>
-            <input type="radio" name="color" id="yellow" />
-            <label for="yellow">
-              <span class="yellow"></span>
-            </label>
-            <input type="radio" name="color" id="olive" />
-            <label for="olive">
-              <span class="olive"></span>
-            </label>
-            <input type="radio" name="color" id="orange" />
-            <label for="orange">
-              <span class="orange"></span>
-            </label>
-            <input type="radio" name="color" id="teal" />
-            <label for="teal">
-              <span class="teal"></span>
-            </label>
-            <input type="radio" name="color" id="blue" />
-            <label for="blue">
-              <span class="blue"></span>
-            </label>
-            <input type="radio" name="color" id="violet" />
-            <label for="violet">
-              <span class="violet"></span>
-            </label>
-            <input type="radio" name="color" id="purple" />
-            <label for="purple">
-              <span class="purple"></span>
-            </label>
-            <input type="radio" name="color" id="pink" />
-            <label for="pink">
-              <span class="pink"></span>
+            <input type="radio" name="color" id="black" />
+            <label for="black">
+              <span class="black"></span>
             </label>
           </div>
           <div className="size-group">
--- src/components/shared/card/CardSummary.js
+++ src/components/shared/card/CardSummary.js
@@ -26,68 +26,18 @@ const CardSummary = ({ path }) => {
             praesentium tempore perspiciatis accusamus amet sed repellendus non,
             distinctio molestiae. Quos quo ducimus ratione.
           </p>
-          {/* <div className="size-group">
-            <p>Size:</p>
-            <select className="size">
-              <option value="">Select Size</option>
-              <option value="XS">XS</option>
-              <option value="S">S</option>
-              <option value="M">M</option>
-              <option value="L">L</option>
-              <option value="XL">XL</option>
-            </select>
-          </div>
-          <div className="quantity-group">
-            <p>Quantity:</p>
-            <input className="quantity" max="9999" min="1" type="number" />
-          </div>
-          <button type="button" className="btn btn-primary">
-            Add to cart
-          </button> */}
+
           <div className="colors">
             Colors:
-            <input type="radio" name="color" id="red" value="red" />
             <label for="red">
               <span class="red"></span>
             </label>
-            <input type="radio" name="color" id="green" />
-            <label for="green">
-              <span class="green"></span>
-            </label>
-            <input type="radio" name="color" id="yellow" />
-            <label for="yellow">
-              <span class="yellow"></span>
-            </label>
-            <input type="radio" name="color" id="olive" />
-            <label for="olive">
-              <span class="olive"></span>
-            </label>
-            <input type="radio" name="color" id="orange" />
-            <label for="orange">
-              <span class="orange"></span>
-            </label>
-            <input type="radio" name="color" id="teal" />
-            <label for="teal">
-              <span class="teal"></span>
-            </label>
-            <input type="radio" name="color" id="blue" />
-            <label for="blue">
-              <span class="blue"></span>
-            </label>
-            <input type="radio" name="color" id="violet" />
-            <label for="violet">
-              <span class="violet"></span>
-            </label>
-            <input type="radio" name="color" id="purple" />
-            <label for="purple">
-              <span class="purple"></span>
-            </label>
-            <input type="radio" name="color" id="pink" />
-            <label for="pink">
-              <span class="pink"></span>
+            <input type="radio" name="color" id="black" />
+            <label for="black">
+              <span class="black"></span>
             </label>
           </div>
-          <Link to="products/product/1">View Full</Link>
+          <Link to="products/product/1">More Detail</Link>
         </div>
       </div>
     </>
