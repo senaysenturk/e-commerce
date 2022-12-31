65b4b8319c1470dffefd297c29256e8c25b9beaf
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-01-01 00:44:18 +0300

    add auth methods

--- src/network/services/baseService.js
+++ src/network/services/baseService.js
@@ -20,6 +20,15 @@ baseService.get = async (url, endpoint) => {
   }
 };
 
+baseService.delede = async (url, endpoint, id) => {
+  try {
+    const response = await axios.delete(`${url}${endpoint}${id}`);
+    return response;
+  } catch (error) {
+    console.log(error.status);
+  }
+};
+
 baseService.postImage = async (url, data) => {
   try {
     const response = await axios.post(url, data, {
@@ -32,7 +41,26 @@ baseService.postImage = async (url, data) => {
     console.log(error.status);
   }
 };
+/*
+axios.interceptors.request.use(
+  function (config) {
+    const { origin } = new URL(config.url);
 
+    const allowedOrigins = ["http://localhost:5500/"];
+    const token = localStorage.getItem("access-token");
+
+    if (allowedOrigins.includes(origin)) {
+      config.headers.authorization = token;
+    }
+
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+*/
 baseService.postAuth = async (url, endpoint, data) => {
   try {
     const response = await axios.post(
@@ -43,6 +71,17 @@ baseService.postAuth = async (url, endpoint, data) => {
         withCredentials: true,
       }
     );
+    return response;
+  } catch (error) {
+    console.log(error.status);
+  }
+};
+
+baseService.postLogout = async (url, endpoint) => {
+  try {
+    const response = await axios.post(`${url}${endpoint}`, {
+      refresh_token: localStorage.getItem("refresh-token"),
+    });
     return response;
   } catch (error) {
     console.log(error.status);
