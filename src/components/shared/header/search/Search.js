fd130e4ed0ce8d5c7f13d6e3d170493dafe32e14
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-01-04 23:39:25 +0300

    Filtering product #46

--- src/components/shared/header/search/Search.js
+++ src/components/shared/header/search/Search.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { useProduct } from "../../../../contexts/product/CreateProductContext";
 import "./style.scss";
 import { AiOutlineSearch, AiOutlineUser } from "react-icons/ai";
 
 const Search = () => {
+  const { products, getAllProducts } = useProduct();
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    const normalizedSearchTerm = event.target.value.trim().toLowerCase();
+   /*  if (normalizedSearchTerm) {
+      setSearchResult(
+        products.filter((product) =>
+          Object.values(product).some((value) =>
+            value.trim().toLowerCase().includes(normalizedSearchTerm)
+          )
+        )
+      );
+    } else {
+      setSearchResult([]);
+    } */
+  };
+
+
+
   return (
     <div className="search">
       <span className="search-icon">
         <AiOutlineSearch />
       </span>
-      <input type="text" />
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={handleSearch}
+        value={searchTerm}
+      />
     </div>
   );
 };
