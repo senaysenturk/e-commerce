0eb624960763f4bfb506694f0886a9edea3b1910
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-01-05 22:16:44 +0300

    Man page #67

--- src/pages/dashboard/man/Man.js
+++ src/pages/dashboard/man/Man.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 import { useProduct } from "../../../contexts/product/CreateProductContext";
 
 export const Man = () => {
