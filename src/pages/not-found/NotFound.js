546ef3439200cb11518e7445dbd3e8bdf3b2a173
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-01-05 03:08:08 +0300

    #70 not-found

--- src/pages/not-found/NotFound.js
+++ src/pages/not-found/NotFound.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./style.scss";
 
 export const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <>
+      <div class="face">
+        <div class="band">
+          <div class="red"></div>
+          <div class="white"></div>
+          <div class="blue"></div>
+        </div>
+        <div class="eyes"></div>
+        <div class="dimples"></div>
+        <div class="mouth"></div>
+      </div>
+
+      <h1>Oops!...</h1>
+      {/* <h1>404</h1> */}
+      <h1>Not Found</h1>
+      <h1>Something went wrong!</h1>
+      <Link to="/">
+        <div class="btn">Home</div>
+      </Link>
+    </>
+  );
+};
 
 export default NotFound;
