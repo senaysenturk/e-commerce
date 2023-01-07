551ecf57e0c6fa722311d63fb22ba7c21603d064
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-01-07 21:07:03 +0300

    Edit Address #62 started

--- src/components/app/shopping-cart/address-form/Popup.js
+++ src/components/app/shopping-cart/address-form/Popup.js
@@ -1,19 +1,52 @@
 import React from "react";
+import { useState } from "react";
+import MaskInput from "react-maskinput";
 
 const Popup = (props) => {
+  const [addAddress, setAddAddress] = useState({});
+
+  const [city, setCity] = useState();
+  const [cities, setCities] = useState([]);
+  const [states, setStates] = useState([]);
+  const [enable, setEnable] = useState(true);
+
+  const getStates = async (city) => {
+    setStates(cities.filter((cityObject) => cityObject.city === city));
+    // console.log("state", states);
+  };
+
+  const handleAddress = (e) => {
+    // console.log(addAddress);
+    setAddAddress({ ...addAddress, [e.target.name]: e.target.value });
+
+    if (e.target.name === "city") {
+      setCity(e.target.value);
+      getStates(city);
+      setEnable(false);
+    }
+  };
+
+  const handleSave = async (e) => {
+    /* var response = await addressInfo(addAddress);
+    getAddresses();
+    handleSetDisplay(); */
+  };
+
   return (
     <div className="popup-box">
       <div className="box">
         <span className="close-icon" onClick={props.handleClose}>
           x
         </span>
+        {console.log(props.currentAddress)}
         <div className="address-form">
           <div className="person">
             <input
               type="text"
               id="fname"
               name="firstname"
-              placeholder="Your name.."
+              defaultValue={props.currentAddress.firstname}
+              onChange={handleAddress}
             />
 
             <input
@@ -21,79 +54,49 @@ const Popup = (props) => {
               id="lname"
               name="lastname"
               placeholder="Your last name.."
+              defaultValue={props.currentAddress.lastname}
+              onChange={handleAddress}
             />
           </div>
-          {/* <MaskInput
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            defaultValue={props.currentAddress.phone}
+            onChange={handleAddress}
+          />
+          <MaskInput
             alwaysShowMask
             mask={"+90 (000) 000 - 0000"}
             size={21}
             showMask
             maskChar="_"
             name="phone"
+            defaultValue={props.currentAddress.phone}
             onChange={handleAddress}
-          /> */}
-
-          {/*  
-          <input
-            type="text"
-            id="phone"
-            name="phone"
-            placeholder="Your phone number.."
-            onChange={handleAddress}
-          /> */}
+          />
           <p>Address Informations</p>
-         {/*  <select id="city" name="city" onChange={handleAddress}>
-            <option>-- None --</option>
-            {cities.map((cityObject, index) => (
-              <option key={index}>{cityObject.city}</option>
-            ))}
-          </select>
-          <select
-            id="state"
-            name="state"
-            disabled={enable}
-            onChange={handleAddress}
-          >
-            <option>-- None --</option>
-
-            {city &&
-              cities
-                .filter((cityName) => cityName.city === city)[0]
-                .states.map((state, index) => {
-                  return <option key={index}>{state}</option>;
-                })}
-          </select> */}
+      <select id="city" name="city" onChange={handleAddress}>
+        <option>-- None --</option>
+        {cities.map((cityObject, index) => (
+          <option key={index}>{cityObject.city}</option>
+        ))}
+      </select>
           <textarea
             id="address"
             name="address"
-            placeholder="Write something.."
+            defaultValue={props.currentAddress.address}
+            onChange={handleAddress}
           ></textarea>
-          {/*  
-          <input
-            type="text"
-            id="postcode"
-            name="postcode"
-            placeholder="Your postal code.."
-            onChange={handleAddress}
-          /> */}
-          {/* <MaskInput
-            alwaysShowMask
-            mask={"00000"}
-            size={6}
-            showMask
-            maskChar="_"
-            name="postcode"
-            onChange={handleAddress}
-          /> */}
-
           <input
             type="text"
             id="addressName"
             name="addressName"
-            placeholder="Your address name.."
+            defaultValue={props.currentAddress.addressName}
+            onChange={handleAddress}
           />
           <div className="add-button">
-            <button className="btn btn-primary">
+            <button className="btn btn-primary" onClick={handleSave}>
               Save
             </button>
           </div>
