9b192afd251be22c3a4a8650abadd233e40002a7
hicranertugral <hicranertugral@gmail.com>
2023-01-08 15:17:40 +0300

    #56 Order Tracking time format

--- src/contexts/basket/GlobalState.js
+++ src/contexts/basket/GlobalState.js
@@ -77,15 +77,7 @@ const GlobalState = (props) => {
         },
         getDate: (format) => {
           const newDate = new Date();
-          const orderTimeAndDateFormat = newDate.toLocaleString(undefined, {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            timeZone: "UTC",
-            ...format
-          });
+          const orderTimeAndDateFormat = newDate.toUTCString();
           return orderTimeAndDateFormat;
         },
       
