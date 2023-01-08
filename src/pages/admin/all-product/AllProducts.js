297855a8af61b0ebd7f2bd9a01b485c759321fbf
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-01-08 20:32:28 +0300

    all product backend

--- src/pages/admin/all-product/AllProducts.js
+++ src/pages/admin/all-product/AllProducts.js
@@ -1,25 +1,65 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import SmallCard from "src/components/app/product/small-card/SmallCard";
 import FilterNavigation from "src/components/shared/filter-navigation/FilterNavigation";
-import ShopContext from "src/contexts/basket/ShopContext";
+import Table from "../../../components/shared/table/Table";
+import ShopContext from "../../../contexts/basket/ShopContext";
 import "./style.scss";
 
 const AllProducts = () => {
   const context = useContext(ShopContext);
   return (
-    <div className="all-product-list">
-      <FilterNavigation></FilterNavigation>
-      <div className="products">
-        <div className="row-header">
-          <h2>All Products</h2>
-        </div>
-        <div className="row">
-          {context.products.map((product, index) => (
+    <>
+      <div className="p-4">
+        <Table
+          searchable={true}
+          head={[
+            { name: "Image" },
+            { name: "Name", sortable: true },
+            { name: "Price", sortable: true },
+            { name: "Size" },
+            { name: "Color" },
+            { name: "Category", sortable: true },
+            { name: "Sub-Category", sortable: true },
+            { name: "Options", width: 200 },
+          ]}
+          // head={Object.keys(context.products[0])}
+          body={context.products && context.products.map((product, key) => [
+            <img src={product.imgPath} />,
+            product.name,
+            product.price + " $",
+            product.size,
+            product.color,
+            product.category,
+            product.subcategory,
+            [
+              <button>Edit</button>,
+              <button
+                onClick={() => {
+                  /* const tmpProducts = [...context.products];
+                  tmpProducts.splice(key, 1);
+                  context.setProducts(tmpProducts); */
+                }}
+              >
+                Delete
+              </button>,
+            ],
+          ])}
+        />
+      </div>
+      {/* <div className="all-product-list">
+        <FilterNavigation></FilterNavigation>
+        <div className="products">
+          <div className="row-header">
+            <h2>All Products</h2>
+          </div>
+          <div className="row">
+            {context.products.map((product, index) => (
             <SmallCard product={product} key={index} />
           ))}
+          </div>
         </div>
-      </div>
-    </div>
+      </div> */}
+    </>
   );
 };
 
