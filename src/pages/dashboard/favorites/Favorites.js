2418eddd7e58f4e7d15e9dba07e8ffa06e475f24
Şenay ŞENTÜRK <senaysenturk@outlook.com>
2023-01-16 02:19:58 +0300

    added empty favorite products info

--- src/pages/dashboard/favorites/Favorites.js
+++ src/pages/dashboard/favorites/Favorites.js
@@ -3,28 +3,42 @@ import "./style.scss";
 import SmallCard from "../../../components/app/product/small-card/SmallCard";
 import { useAuth } from "src/contexts/auth/AuthContext";
 import FilterNavigation from "src/components/shared/filter-navigation/FilterNavigation";
+import { useNavigate } from "react-router-dom";
 
 const Favorites = () => {
   const { favorites, setFavorites, getUserFavorites } = useAuth();
+  const navigate = useNavigate();
   const [filterKeys, setFilterKeys] = useState("");
   useEffect(() => {
     getUserFavorites();
   }, []);
   return (
     <>
-      <div className="search-and-filter-list">
-        <div className="products">
-          <div className="row-header">
-            <h2>Favorites</h2>
-          </div>
-          <div className="search-row">
-            {favorites &&
-              favorites.map((product, index) => (
+      {favorites.length ? (
+        <div className="search-and-filter-list">
+          <div className="products">
+            <div className="row-header">
+              <h2>Favorites</h2>
+            </div>
+            <div className="search-row">
+              {favorites.map((product, index) => (
                 <SmallCard product={product} key={index} />
               ))}
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="cart-empty">
+          <div className="empty-message">
+            <p className="danger">No favorites found!</p>
+
+            <p>Add your products to your favorites and start shopping.</p>
+          </div>
+          <button className="btn btn-gray" onClick={() => navigate("/")}>
+            Start Shopping
+          </button>
+        </div>
+      )}
     </>
   );
 };
