27f4b4879daa31fccbac4f2fb366e893dc4d13b7
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-01-16 16:42:06 +0300

    show order detail

--- src/components/app/admin/order/edit-popup/EditPopup.js
+++ src/components/app/admin/order/edit-popup/EditPopup.js
@@ -2,15 +2,45 @@ import React from "react";
 import { useContext, useState, useEffect } from "react";
 import "./style.scss";
 import { AuthContext } from "../../../../../contexts/auth/AuthContext";
+import { useProduct } from "../../../../../contexts/product/CreateProductContext";
 
 export const EditPopup = ({ orderObject, handleClose }) => {
   const authContext = useContext(AuthContext);
+  const {
+    updateProduct,
+    image,
+    setImage,
+    uploadImage,
+    imageURL,
+    colors,
+    getAllColors,
+    sizes,
+    getAllSizes,
+    categories,
+    getAllCategories,
+    additionalCategories,
+    getAllAdditionalCategories,
+    uploadPercentage,
+    // setColor,
+    // setSize,
+  } = useProduct();
   const [orderDate, setOrderDate] = useState(
     orderObject.date.split(",").slice(0, 1).toString()
   );
   const [user, setUser] = useState(
     authContext.users.find((user) => user.id === orderObject.userId)
   );
+
+  const [color, setColor] = useState([]);
+  const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    getAllColors();
+    getAllSizes();
+    getAllCategories("categories");
+    getAllAdditionalCategories("additionalCategories");
+  }, []);
+
   return (
     <>
       <div className="popup-box">
@@ -70,15 +100,234 @@ export const EditPopup = ({ orderObject, handleClose }) => {
                 orderObject.orderList.map((order, i) => {
                   return (
                     <>
-                      <label htmlFor="user">Product Name</label>
+                      <label htmlFor="product-name">Product Name</label>
                       <input
                         type="text"
-                        placeholder="Enter a profile name."
-                        id="user"
-                        name="user"
+                        placeholder="T-Shirt"
+                        id="name"
+                        name="name"
                         defaultValue={order.name}
+                        // onChange={handleProduct}
+                      />
+                      <label htmlFor="product-detail">Product Detail</label>
+                      <input
+                        type="text"
+                        placeholder="Lorem, ipsum dolor sit amet consectetur adipisicing elit. "
+                        id="detail"
+                        name="detail"
+                        defaultValue={order.detail}
+                        // onChange={handleProduct}
+                      />
+
+                      <label htmlFor="price">Price</label>
+                      <input
+                        type="number"
+                        placeholder="10.99$"
+                        step="0.01"
+                        min="0"
+                        id="price"
+                        name="price"
+                        defaultValue={order.price}
                         // onChange={handleUser}
                       />
+
+                      <div className="multi-choices">
+                        <p>Sizes:</p>
+                        {sizes.map((size, index) =>
+                          order.size.includes(size) ? (
+                            <div className="choice">
+                              <input
+                                type="checkbox"
+                                name="size"
+                                id={size}
+                                value={size}
+                                defaultChecked={color}
+                                // onChange={handleSetSize}
+                              />
+                              <label htmlFor={size}>{size}</label>
+                            </div>
+                          ) : (
+                            <div className="choice">
+                              <input
+                                type="checkbox"
+                                name="size"
+                                id={size}
+                                value={size}
+                                // onChange={handleProduct}
+                              />
+                              <label htmlFor={size}>{size}</label>
+                            </div>
+                          )
+                        )}
+                      </div>
+
+                      <div className="colors">
+                        <p>Colors:</p>
+                        <div className="color">
+                          {colors.map((color, index) =>
+                            order.color.includes(color) ? (
+                              <>
+                                <input
+                                  type="checkbox"
+                                  name="color"
+                                  id={color}
+                                  value={color}
+                                  defaultChecked={color}
+                                  // onChange={handleProduct}
+                                />
+                                <label htmlFor={color}>
+                                  <span
+                                    className={color}
+                                    name={color}
+                                    id={color}
+                                  ></span>
+                                </label>
+                              </>
+                            ) : (
+                              <>
+                                <input
+                                  type="checkbox"
+                                  name="color"
+                                  id={color}
+                                  value={color}
+                                  // onChange={handleProduct}
+                                />
+                                <label htmlFor={color}>
+                                  <span
+                                    className={color}
+                                    name={color}
+                                    id={color}
+                                  ></span>
+                                </label>
+                              </>
+                            )
+                          )}
+                        </div>
+                      </div>
+
+                      <label htmlFor="category">Category</label>
+                      <select
+                        id="category"
+                        name="category"
+                        // onChange={handleProduct}
+                      >
+                        {categories.map((categoryObj, index) =>
+                          categoryObj.category === order.category ? (
+                            <option
+                              value={categoryObj.category}
+                              key={index}
+                              selected
+                            >
+                              {categoryObj.category}
+                            </option>
+                          ) : (
+                            <option value={categoryObj.category} key={index}>
+                              {categoryObj.category}
+                            </option>
+                          )
+                        )}
+                      </select>
+
+                      <label htmlFor="category">Subcategory</label>
+                      <select
+                        id="subcategory"
+                        name="subcategory"
+                        // onChange={handleProduct}
+                      >
+                        <option>-- None --</option>
+                        {!category
+                          ? categories.filter(
+                              (categoryObj, index) =>
+                                categoryObj.category === order.category
+                            )[0] &&
+                            categories
+                              .filter(
+                                (productCategory) =>
+                                  productCategory.category == order.category
+                              )[0]
+                              .subcategory.map((subCategory, index) =>
+                                subCategory === order.subcategory ? (
+                                  <option
+                                    value={subCategory}
+                                    key={index}
+                                    selected
+                                  >
+                                    {subCategory}
+                                  </option>
+                                ) : (
+                                  <option value={subCategory} key={index}>
+                                    {subCategory}
+                                  </option>
+                                )
+                              )
+                          : categories
+                              .filter(
+                                (productCategory) =>
+                                  productCategory.category == category
+                              )[0]
+                              .subcategory.map((subCategory, index) =>
+                                subCategory === order.setcategory ? (
+                                  <option
+                                    value={subCategory}
+                                    key={index}
+                                    selected
+                                  >
+                                    {subCategory}
+                                  </option>
+                                ) : (
+                                  <option value={subCategory} key={index}>
+                                    {subCategory}
+                                  </option>
+                                )
+                              )}
+                      </select>
+
+                      <div className="multi-choices">
+                        <p>Additional categories:</p>
+                        {additionalCategories.map((additionalCategory, index) =>
+                          order.additionalCategories.includes(
+                            additionalCategory
+                          ) ? (
+                            <div className="choice">
+                              <input
+                                type="checkbox"
+                                name="additional-category"
+                                id={additionalCategory}
+                                value={additionalCategory}
+                                defaultChecked={additionalCategory}
+                                // onChange={handleSetSize}
+                              />
+                              <label htmlFor={additionalCategory}>
+                                {additionalCategory}
+                              </label>
+                            </div>
+                          ) : (
+                            <div className="choice">
+                              <input
+                                type="checkbox"
+                                name="additional-category"
+                                id={additionalCategory}
+                                value={additionalCategory}
+                                // onChange={handleProduct}
+                              />
+                              <label htmlFor={additionalCategory}>
+                                {additionalCategory}
+                              </label>
+                            </div>
+                          )
+                        )}
+                      </div>
+
+                      <label htmlFor="image">Image</label>
+                      <img src={order.imgPath} />
+                      <label>{order.imgPath}</label>
+                      <input
+                        type="file"
+                        id="image"
+                        name="image"
+                        // onChange={handleImage}
+                      />
+                      {/* <ProgressBar bgcolor={"#6a1b9a"} completed={uploadPercentage} /> */}
                     </>
                   );
                 })}
