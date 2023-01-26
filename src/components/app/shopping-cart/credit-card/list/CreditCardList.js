ba52b0b319b43197bf2d1c63d30046685120931a
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-01-26 18:00:52 +0300

    added credit card list actions

--- src/components/app/shopping-cart/credit-card/list/CreditCardList.js
+++ src/components/app/shopping-cart/credit-card/list/CreditCardList.js
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useState } from "react";
+
+import { HiOutlinePlus } from "react-icons/hi";
+import { AuthContext } from "../../../../../contexts/auth/AuthContext";
+import ShopContext from "src/contexts/basket/ShopContext";
+import Popup from "../popup/Popup";
 
 export const CreditCardList = ({ handleDisplay, hide }) => {
-  return <div>CreditCardList</div>;
+  const authContext = useContext(AuthContext);
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentCard, setCurrentCard] = useState([]);
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleDeleteCard = async (cardName) => {
+    authContext.deleteCreditCard(cardName);
+  };
+
+  return (
+    <>
+      <div className="add-new-card">
+        <HiOutlinePlus />
+        <a onClick={handleDisplay}> New Credit Card</a>
+      </div>
+      <div className="checkout-card">
+        {authContext.user[0].cards &&
+          authContext.user[0].cards.map((cardObject, index) => {
+            return (
+              <div className="checkout-card-box" key={index}>
+                <div className="card">
+                  <div className="card-name">
+                    <input
+                      type="radio"
+                      name="card-name"
+                      id="card-name"
+                      onChange={() => authContext.setOrderCard(cardObject)}
+                    />
+                    <label htmlFor="card-name">
+                      <h4>{cardObject.cardName}</h4>
+                    </label>
+                  </div>
+                  <div className="card-content">
+                    <span>{cardObject.cardNumber}</span>
+                    <span>{cardObject.cardNumber.slice(0, 7).concat("** **** ", cardObject.cardNumber.slice(-4))}</span>
+                    <span>
+                      {cardObject.expirationMonth} / {cardObject.expirationYear}
+                    </span>
+                  </div>
+                </div>
+                <div className="edit">
+                  <span
+                    onClick={() => {
+                      togglePopup();
+                      setCurrentCard(cardObject);
+                    }}
+                  >
+                    Edit
+                  </span>
+                </div>
+                <div className="divider">
+                  <span>|</span>
+                </div>
+                <div className="delete">
+                  <span onClick={() => handleDeleteCard(cardObject.cardName)}>
+                    Delete
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+      {isOpen && <Popup handleClose={togglePopup} currentCard={currentCard} />}
+    </>
+  );
 };
 
 export default CreditCardList;
